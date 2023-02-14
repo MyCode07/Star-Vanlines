c5151404bbb1ac1e7f6fbb245de9241afd48e0e3
MyCode07 <erem19992015@mail.ru>
2023-02-14 20:09:12 +0300

    author page

--- src/js/utils/header-search.js
+++ src/js/utils/header-search.js
@@ -1,9 +1,23 @@
 const headerSearch = document.querySelector('.header__search');
-
 if (headerSearch) {
     const input = headerSearch.querySelector('input');
     const button = headerSearch.querySelector('button');
+    button.disabled = true
+    input.addEventListener('input', function () {
+        if (input.value != '') {
+            button.disabled = false
+        }
+        else {
+            button.disabled = true
+        }
+    })
+}
 
+const searchSearch = document.querySelector('.search__search');
+if (searchSearch) {
+    const input = searchSearch.querySelector('input');
+    const button = searchSearch.querySelector('button');
+    button.disabled = true
     input.addEventListener('input', function () {
         if (input.value != '') {
             button.disabled = false
--- src/js/utils/stiky-header.js
+++ src/js/utils/stiky-header.js
@@ -1,5 +1,3 @@
-import gsap from "gsap";
-
 let scrollY = window.scrollY;
 
 window.addEventListener('scroll', function () {
@@ -11,11 +9,22 @@ stikyHeader()
 
 function stikyHeader() {
     const header = document.querySelector('.header');
+    const search = document.querySelector('#is-ajax-search-result-908');
 
     if (scrollY > 70) {
         header.classList.add('_stiky');
+        if (search) {
+            search.style.opacity = 0;
+            search.style.visibility = 'hidden';
+            search.style.pointerEvents = 'none';
+        }
     }
     else {
         header.classList.remove('_stiky');
+        if (search) {
+            search.style.opacity = 1;
+            search.style.visibility = 'visible';
+            search.style.pointerEvents = 'all';
+        }
     }
 }
